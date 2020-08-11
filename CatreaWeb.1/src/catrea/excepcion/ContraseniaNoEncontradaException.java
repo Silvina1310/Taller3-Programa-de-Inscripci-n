3b02f9e1ac362cd6229d424731ada39a02746156
Silvina1310 <49768696+Silvina1310@users.noreply.github.com>
2020-08-11 15:27:30 -0300

    *

--- CatreaWeb.1/src/catrea/excepcion/ContraseniaNoEncontradaException.java
+++
@@ -1,8 +0,0 @@
-package catrea.excepcion;
-
-public class ContraseniaNoEncontradaException extends Exception {
-    public ContraseniaNoEncontradaException(String msg) {//constructor
-        super(msg);
-    }
-}
-    
